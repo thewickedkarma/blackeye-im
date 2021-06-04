c6f0ab36fbe25e4ce191e43aa3e3e9b411e0272a
AnonymousDebug <kakudaku2009@gmail.com>
2021-06-04 22:59:17 +0530

    Updated blackeye.sh
    Replaced Install it using 'apt install tool' to 'sudo apt install tool'.

--- blackeye.sh
+++ blackeye.sh
@@ -7,10 +7,10 @@ trap 'printf "\n";stop;exit 1' 2
 
 dependencies() {
 
-command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it by 'apt install php'. Aborting."; exit 1; }
-command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it 'apt install wget'. Aborting."; exit 1; }
-command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it 'apt install unzip'. Aborting."; exit 1; }
-command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it 'apt install curl'. Aborting."; exit 1; }
+command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it by 'sudo apt install php'. Aborting."; exit 1; }
+command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it 'sudo apt install wget'. Aborting."; exit 1; }
+command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it 'sudo apt install unzip'. Aborting."; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it 'sudo apt install curl'. Aborting."; exit 1; }
 
 }
 
