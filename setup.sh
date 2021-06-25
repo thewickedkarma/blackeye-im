e32f4df7d174c20e3d723b1bfeb088c3c3659b77
The Burning <62335141+Git-Ankitraj@users.noreply.github.com>
2021-06-25 17:09:58 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@ declare -A osInfo;
 osInfo[/etc/redhat-release]=yum
 osInfo[/etc/arch-release]=pacman
 osInfo[/etc/gentoo-release]=emerge
-osInfo[/etc/SuSE-release]=zypp
 osInfo[/etc/debian_version]=apt-get
 
 for f in ${!osInfo[@]}
@@ -10,8 +9,17 @@ do
     if [[ -f $f ]];then
         echo ${osInfo[$f]} detected..
         echo Installing dependencies...
+        if ((${osInfo[$f]} == apt-get)); then
+            sudo apt-get install wget curl php unzip -y
+        fi
         if ((${osInfo[$f]} == pacman)); then
             sudo pacman -S wget curl php unzip --noconfirm
+        fi
+        if ((${osInfo[$f]} == yum)); then
+            sudo yum install wget curl php unzip
+        fi
+        if ((${osInfo[$f]} == emerge )); then
+            sudo emerge -u wget curl php unzip
         fi
 
     fi
