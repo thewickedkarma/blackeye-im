bf21cd25545cbaa8ebb2974794a9999faa9b403e
Levi Hackerman <62335141+The-Burning@users.noreply.github.com>
2022-01-24 17:49:34 +0530

    v2.5

--- setup.sh
+++ setup.sh
@@ -15,17 +15,21 @@ do
         echo Installing dependencies...
         if ((${osInfo[$f]} == apt-get)); then
             sudo apt-get install wget curl php unzip lolcat figlet npm nodejs jq xsel -y
+            npm install -g localtunnel
         fi
         if ((${osInfo[$f]} == pacman)); then
             sudo pacman -S wget curl php unzip figlet npm nodejs jq xsel --noconfirm
             sudo pacman -S gem
             sudo gem install lolcat
+            npm install -g localtunnel
         fi
         if ((${osInfo[$f]} == yum)); then
             sudo yum install wget curl php unzip lolcat figlet npm nodejs jq xsel
+            npm install -g localtunnel
         fi
         if ((${osInfo[$f]} == emerge )); then
             sudo emerge -u wget curl php unzip lolcat figlet npm nodejs jq xsel
+            npm install -g localtunnel
         fi
 
     fi
